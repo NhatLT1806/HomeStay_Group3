e5c0351231a7de0b016cf99b11553d1e7f0367df
NhatLT1806 <nhatlthe180545@fpt.edu.vn>
2024-07-27 21:12:46 +0700

    add booking controller

--- HomeStay_Group3/src/java/Controller/BookingController.java
+++ HomeStay_Group3/src/java/Controller/BookingController.java
@@ -5,7 +5,9 @@
 package Controller;
 
 import DAO.HomestayDAO;
+import DAO.NotificationDAO;
 import DAO.RoomDAO;
+import Model.BookingRequest;
 import Model.Homestay;
 import Model.Room;
 import Model.User;
@@ -54,6 +56,9 @@ public class BookingController extends HttpServlet {
                     case "reject-booking":
                         RejectBooking(request, response);
                         break;
+                    case "cancle-booking":
+                        CancleBooking(request, response);
+                        break;
                 }
             } else {
                 response.sendRedirect(LOGIN_PAGE);
@@ -95,6 +100,22 @@ public class BookingController extends HttpServlet {
                 RoomDAO roomDAO = new RoomDAO();
                 boolean result = roomDAO.BookingRoom(roomId, user.getId());
                 if (result) {
+
+                    NotificationDAO notiDAO = new NotificationDAO();
+                    // tạo noti cho book room
+
+                    Room room = roomDAO.GetRoomById(roomId);
+                    String title = "YÊU CẦU THUÊ PHÒNG";
+                    String contentNoti = "Yêu cầu thuê phòng " + room.getName() + " của bạn đã được gửi đi vui lòng chờ được duyệt!";
+                    notiDAO.createNotification(user.getId(), title, contentNoti);
+
+                    HomestayDAO homestayDAO = new HomestayDAO();
+                    int ownerId = homestayDAO.getUserIdByHomeStayId(room.getHomestayId());
+                    title = "YÊU CẦU THUÊ PHÒNG";
+                    contentNoti = room.getName() + " đang có yêu cầu thuê phòng từ " + user.getFirstName() +" "+ user.getLastName();
+                    notiDAO.createNotification(ownerId, title, contentNoti);
+
+                    // gửi yêu cầu cho chủ homestay luôn
                     request.setAttribute("MESSAGE", "Bạn đã đặt phòng roomId - " + roomIds + "thành công ! Vui lòng đợi owner chấp nhận yêu cầu của bạn");
                 } else {
                     request.setAttribute("ERROR", "Bạn đã đặt phòng roomId - " + roomIds + " không thành công ! Hãy thử lại sau");
@@ -104,6 +125,8 @@ public class BookingController extends HttpServlet {
                 if (room != null) {
                     HomestayDAO homestayDAO = new HomestayDAO();
                     Homestay homestay = homestayDAO.GetHomestayById(room.getHomestayId());
+                    User onwer = homestayDAO.GetOnwerByHomestayId(room.getHomestayId());
+                    request.setAttribute("OWNER", onwer);
                     request.setAttribute("HOMESTAY", homestay);
                     request.setAttribute("SERVICES", listServices);
                     request.setAttribute("ROOM", room);
@@ -119,13 +142,32 @@ public class BookingController extends HttpServlet {
         try {
             HttpSession session = request.getSession();
             User user = (User) session.getAttribute("USER");
+            String statusS = request.getParameter("status");
+            String indexS = request.getParameter("index");
+            if (indexS == null) {
+                indexS = "1";
+            }
+            if (statusS == null || statusS == "") {
+                // neu truong hop k truyen gi hoac vao mac dinh se lay het request.
+                statusS = "4";
+            }
+            int index = Integer.parseInt(indexS);
+            int status = Integer.parseInt(statusS);
             RoomDAO roomDAO = new RoomDAO();
-            List<BookingVM> listBooking = roomDAO.getListRequestToOwner(user.getId());
+            int total = roomDAO.getListRequestToOwnerTotal(user.getId(), status);
+            List<BookingVM> listBooking = roomDAO.getListRequestToOwner(user.getId(), status, index);
             if (listBooking != null) {
                 request.setAttribute("BOOKINGS", listBooking);
             } else {
                 System.out.println("Khong co yeu cau booking");
             }
+            int lastPage = total / 8;
+            if (total % 8 != 0) {
+                lastPage++;
+            }
+            request.setAttribute("endP", lastPage);
+            request.setAttribute("selectedPage", index);
+            request.setAttribute("status", status);
             request.getRequestDispatcher(MANAGE_BOOKING).forward(request, response);
         } catch (Exception e) {
             e.printStackTrace();
@@ -136,14 +178,36 @@ public class BookingController extends HttpServlet {
     private void ViewBookingHistory(HttpServletRequest request, HttpServletResponse response) {
         try {
             HttpSession session = request.getSession();
-            User user = (User) session.getAttribute("USER");
             RoomDAO roomDAO = new RoomDAO();
-            List<BookingVM> listBooking = roomDAO.getListRequestToBooker(user.getId());
+            User user = (User) session.getAttribute("USER");
+            String statusS = request.getParameter("status");
+            String indexS = request.getParameter("index");
+            if (indexS == null) {
+                indexS = "1";
+            }
+            if (statusS == null || statusS == "") {
+                // neu truong hop k truyen gi hoac vao mac dinh se lay het request.
+                statusS = "4";
+            }
+            int index = Integer.parseInt(indexS);
+            int status = Integer.parseInt(statusS);
+
+            int total = roomDAO.getListRequestToBookerTotal(user.getId(), status);
+            List<BookingVM> listBooking = roomDAO.getListRequestToBooker(user.getId(), status, index);
+
             if (listBooking != null) {
                 request.setAttribute("BOOKINGS", listBooking);
             } else {
                 System.out.println("Khong co yeu cau booking");
             }
+
+            int lastPage = total / 8;
+            if (total % 8 != 0) {
+                lastPage++;
+            }
+            request.setAttribute("endP", lastPage);
+            request.setAttribute("selectedPage", index);
+            request.setAttribute("status", status);
             request.getRequestDispatcher(HISTORY_BOOKING).forward(request, response);
         } catch (Exception e) {
             e.printStackTrace();
@@ -152,17 +216,28 @@ public class BookingController extends HttpServlet {
 
     private void AcceptBooking(HttpServletRequest request, HttpServletResponse response) {
         try {
-            String requestIds = request.getParameter("requestId"); 
+            String requestIds = request.getParameter("requestId");
             String roomIdS = request.getParameter("roomId");
 
-            int requestId = Integer.parseInt(requestIds);         
+            int requestId = Integer.parseInt(requestIds);
             int roomId = Integer.parseInt(roomIdS);
 
             HttpSession session = request.getSession();
             User user = (User) session.getAttribute("USER");
             RoomDAO roomDAO = new RoomDAO();
+
             boolean result = roomDAO.ConfirmBooking(requestId, roomId);
             if (result) {
+
+                // get booking userId Booking request
+                BookingRequest brq = roomDAO.getRequetsBookingById(requestId);
+                // taoj noti khi duoc thue phong
+                NotificationDAO notiDAO = new NotificationDAO();
+                Room room = roomDAO.GetRoomById(roomId);
+                String title = "YÊU CẦU THUÊ PHÒNG";
+                String contentNoti = "Yêu cầu thuê phòng " + room.getName() + " của bạn đã được duyệt!";
+                notiDAO.createNotification(brq.getBookBy(), title, contentNoti);
+
                 request.setAttribute("MESSAGE", "Phòng id - " + roomIdS + " đã được duyệt thuê thành công");
             } else {
                 System.out.println("EROR at accept Booking");
@@ -175,10 +250,10 @@ public class BookingController extends HttpServlet {
 
     private void RejectBooking(HttpServletRequest request, HttpServletResponse response) {
         try {
-            String requestIds = request.getParameter("requestId"); 
+            String requestIds = request.getParameter("requestId");
             String roomIdS = request.getParameter("roomId");
 
-            int requestId = Integer.parseInt(requestIds);         
+            int requestId = Integer.parseInt(requestIds);
             int roomId = Integer.parseInt(roomIdS);
 
             HttpSession session = request.getSession();
@@ -186,11 +261,40 @@ public class BookingController extends HttpServlet {
             RoomDAO roomDAO = new RoomDAO();
             boolean result = roomDAO.RejectBooking(requestId, roomId);
             if (result) {
-                request.setAttribute("MESSAGE", "Cập nhật trạng thái phòng thành công" );
+
+                // get booking userId Booking request
+                BookingRequest brq = roomDAO.getRequetsBookingById(requestId);
+                // taoj noti khi duoc thue phong
+                NotificationDAO notiDAO = new NotificationDAO();
+                Room room = roomDAO.GetRoomById(roomId);
+                String title = "YÊU CẦU THUÊ PHÒNG";
+                String contentNoti = "Yêu cầu thuê phòng " + room.getName() + " của bạn đã bị từ chối!";
+                notiDAO.createNotification(brq.getBookBy(), title, contentNoti);
+                request.setAttribute("MESSAGE", "Cập nhật trạng thái phòng thành công");
+                
             } else {
                 System.out.println("EROR at accept Booking");
             }
             request.getRequestDispatcher("book?action=view-room-booking").forward(request, response);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void CancleBooking(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            String requestIds = request.getParameter("requestId");
+            int requestId = Integer.parseInt(requestIds);
+            HttpSession session = request.getSession();
+            User user = (User) session.getAttribute("USER");
+            RoomDAO roomDAO = new RoomDAO();
+            boolean result = roomDAO.CancleBooking(requestId);
+            if (result) {
+                request.setAttribute("MESSAGE", "Cập nhật trạng thái phòng thành công");
+            } else {
+                System.out.println("EROR at accept Booking");
+            }
+            request.getRequestDispatcher("book?action=view-booking-history").forward(request, response);
         } catch (Exception e) {
             e.printStackTrace();
         }
